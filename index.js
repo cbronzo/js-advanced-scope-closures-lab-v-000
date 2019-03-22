ea3b0e0255a96ef04f0e7b7406f3433c3fe8f7d6
Catherine Bronzo <cbronzo@mac.com>
2019-03-22 04:20:47 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,7 +1,15 @@
 let driverId = 0 
 
-
-
+function produceDrivingRange(range) {
+  return function (street1, street2) {
+    const distance = parseInt(street2) - parseInt(street1)
+    if (distance > range){
+      return `${distance - range} blocks out of range`;
+    } else {
+      return `within range by ${range - distance}`;
+    }
+  }
+}
 
 function produceTipCalculator(percentTipSelected){
   return function(totalBill) {
